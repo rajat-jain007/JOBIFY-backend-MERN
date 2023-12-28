e3c64815c61a5f478ba948bd2d38d28f220dead7
rajat-jain007 <rajatbachhawat@gmail.com>
2023-12-28 18:01:59 +0530

    password hashing using bcrypt library

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,19 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
+import bcrypt from "bcryptjs";
 
 // Register
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0; // countDocuments is a method in mongodb
   req.body.role = isFirstAccount ? "admin" : "user"; // .role is coming from UserModel.js component
 
+  // Hashing the password using bcryptjs
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt); // accessing password from UserModel.js
+  req.body.password = hashedPassword;
+
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ msg: "User Created" });
 };
 
 //Login
