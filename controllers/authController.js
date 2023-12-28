2e78534d2160016441660c6a94d1886a45af4e65
rajat-jain007 <rajatbachhawat@gmail.com>
2023-12-28 18:25:28 +0530

    passwordUtils component added for reusability of the component in next project

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
-import bcrypt from "bcryptjs";
+import { hashPassword } from "../utils/passwordUtils.js";
 
 // Register
 export const register = async (req, res) => {
@@ -8,8 +8,8 @@ export const register = async (req, res) => {
   req.body.role = isFirstAccount ? "admin" : "user"; // .role is coming from UserModel.js component
 
   // Hashing the password using bcryptjs
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt); // accessing password from UserModel.js
+
+  const hashedPassword = await hashPassword(req.body.password);
   req.body.password = hashedPassword;
 
   const user = await User.create(req.body);
