51ef6027f304d599ef0437fda37c8f7086df8f0f
rajat-jain007 <rajatbachhawat@gmail.com>
2023-12-29 14:34:39 +0530

    comparePassword & user check

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
-import { hashPassword } from "../utils/passwordUtils.js";
+import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
+import { UnauthenticatedError } from "../errors/customErrors.js";
 
 // Register
 export const register = async (req, res) => {
@@ -19,5 +20,14 @@ export const register = async (req, res) => {
 //Login
 
 export const login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) throw new UnauthenticatedError("Invalid Credentials");
+  const isPasswordCorrect = await comparePassword(
+    req.body.password,
+    user.password
+  );
+
+  if (!isPasswordCorrect) throw new UnauthenticatedError("Wrong Password");
   res.send("login");
 };
